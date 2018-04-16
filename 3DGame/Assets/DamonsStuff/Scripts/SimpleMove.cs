80cc88ff7d89580feba660db7557586d4a3c767a
Damon Fuller <18.damon.fuller@ksd.org>
2018-04-16 13:59:06 -0700

    Working on the doors. DRF

---
+++ 3DGame/Assets/DamonsStuff/Scripts/SimpleMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour {
+    public GameObject showMe;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Oof");
+        if (other.gameObject.tag == "Interactable")
+        {
+            showMe.SetActive(true);
+        }
+    }
+}
