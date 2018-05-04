6b2f83655769f1ba12e9ba5347ab5c8337ad5ca0
Nathan Weis <17.nathan.weis@ksd.org>
2018-05-04 14:49:42 -0700

    NathanNotsoUGH

---
+++ 3DGame/Assets/!Actual Game Stuff/Scripts/MainMenuScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuScript : MonoBehaviour {
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("New Game"), new BaseEventData(EventSystem.current));
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("DamonDemoScene");
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath +
+            "/" + gameObject.name + ".dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath +
+            "/" + gameObject.name + ".dat", FileMode.Open);
+            BasicSaveObj myData = (BasicSaveObj)bf.Deserialize(file);
+
+            //Loading player1 data
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1Name = myData.player1Name;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1HealthPointsMax = myData.player1HPMax;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player1HealthPointsCurrent = myData.player1HPCurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1PhysicalDamage = myData.player1PhysicalDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1PhysicalResist = myData.player1PhysicalResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1TechDamage = myData.player1TechDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1TechResist = myData.player1TechResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1CurrentEXP = myData.player1EXPcurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1NeededEXP = myData.player1EXPneeded;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1Level = myData.player1level;
+
+            //Loading player2 data
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2Name = myData.player2Name;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2HealthPointsMax = myData.player2HPMax;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player2HealthPointsCurrent = myData.player2HPCurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2PhysicalDamage = myData.player2PhysicalDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2PhysicalResist = myData.player2PhysicalResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2TechDamage = myData.player2TechDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2TechResist = myData.player2TechResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2CurrentEXP = myData.player2EXPcurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2NeededEXP = myData.player2EXPneeded;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2Level = myData.player2level;
+
+            //Loading player3 data
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3Name = myData.player3Name;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3HealthPointsMax = myData.player3HPMax;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player3HealthPointsCurrent = myData.player3HPCurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3PhysicalDamage = myData.player3PhysicalDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3PhysicalResist = myData.player3PhysicalResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3TechDamage = myData.player3TechDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3TechResist = myData.player3TechResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3CurrentEXP = myData.player3EXPcurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3NeededEXP = myData.player3EXPneeded;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3Level = myData.player3level;
+
+            //Loading player4 data
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4Name = myData.player4Name;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4HealthPointsMax = myData.player4HPMax;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player4HealthPointsCurrent = myData.player4HPCurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4PhysicalDamage = myData.player4PhysicalDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4PhysicalResist = myData.player4PhysicalResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4TechDamage = myData.player4TechDamage;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4TechResist = myData.player4TechResist;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4CurrentEXP = myData.player4EXPcurrent;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4NeededEXP = myData.player4EXPneeded;
+            GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4Level = myData.player4level;
+
+            transform.position = new Vector3(myData.x, myData.y, myData.z);
+            file.Close();
+
+            SceneManager.LoadScene("DamonDemoScene");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 3DGame/Assets/DamonsStuff/Scripts/SimpleMove.cs
+++ 3DGame/Assets/DamonsStuff/Scripts/SimpleMove.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
 
 public class SimpleMove : MonoBehaviour {
     public GameObject showMe;
@@ -46,6 +49,72 @@ public class SimpleMove : MonoBehaviour {
         isPaused = false;
         Time.timeScale = originTime;
         pauseMenu.SetActive(false);
+    }
+
+    public void SaveData()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        //ASSUMPTION: all objects using this script will have unique names
+        FileStream file = File.Open(Application.persistentDataPath
+            + "/" + gameObject.name + ".dat", FileMode.OpenOrCreate);
+        BasicSaveObj myData = new BasicSaveObj();
+
+        //Saving player1 data
+        myData.player1Name = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1Name;
+        myData.player1HPMax = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1HealthPointsMax;
+        myData.player1HPCurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player1HealthPointsCurrent;
+        myData.player1PhysicalDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1PhysicalDamage;
+        myData.player1PhysicalResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1PhysicalResist;
+        myData.player1TechDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1TechDamage;
+        myData.player1TechResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1TechResist;
+        myData.player1EXPcurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1CurrentEXP;
+        myData.player1EXPneeded = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1NeededEXP;
+        myData.player1level = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player1Level;
+
+        //Saving player2 data
+        myData.player2Name = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2Name;
+        myData.player2HPMax = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2HealthPointsMax;
+        myData.player2HPCurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player2HealthPointsCurrent;
+        myData.player2PhysicalDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2PhysicalDamage;
+        myData.player2PhysicalResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2PhysicalResist;
+        myData.player2TechDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2TechDamage;
+        myData.player2TechResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2TechResist;
+        myData.player2EXPcurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2CurrentEXP;
+        myData.player2EXPneeded = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2NeededEXP;
+        myData.player2level = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player2Level;
+
+        //Saving player3 data
+        myData.player3Name = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3Name;
+        myData.player3HPMax = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3HealthPointsMax;
+        myData.player3HPCurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player3HealthPointsCurrent;
+        myData.player3PhysicalDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3PhysicalDamage;
+        myData.player3PhysicalResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3PhysicalResist;
+        myData.player3TechDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3TechDamage;
+        myData.player3TechResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3TechResist;
+        myData.player3EXPcurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3CurrentEXP;
+        myData.player3EXPneeded = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3NeededEXP;
+        myData.player3level = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player3Level;
+
+        //Saving player4 data
+        myData.player4Name = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4Name;
+        myData.player4HPMax = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4HealthPointsMax;
+        myData.player4HPCurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().Player4HealthPointsCurrent;
+        myData.player4PhysicalDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4PhysicalDamage;
+        myData.player4PhysicalResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4PhysicalResist;
+        myData.player4TechDamage = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4TechDamage;
+        myData.player4TechResist = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4TechResist;
+        myData.player4EXPcurrent = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4CurrentEXP;
+        myData.player4EXPneeded = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4NeededEXP;
+        myData.player4level = GameObject.Find("GameInformation").GetComponent<PlayerGlobalInformation>().player4Level;
+
+        
+        myData.x = transform.position.x;
+        myData.y = transform.position.y;
+        myData.z = transform.position.z;
+        bf.Serialize(file, myData);
+        file.Close();
+
+        SceneManager.LoadScene("MainMenu");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -104,4 +173,61 @@ public class SimpleMove : MonoBehaviour {
         talkToMe.GetComponentInChildren<Typing>().loadScene = other.GetComponent<Talking>().loadScene;
         talkToMe.GetComponentInChildren<Typing>().sceneName = other.GetComponent<Talking>().sceneName;
     }
+}
+
+[System.Serializable]
+public class BasicSaveObj
+{
+    //Player1 stats
+    public string player1Name;
+    public int player1HPMax;
+    public int player1HPCurrent;
+    public int player1PhysicalDamage;
+    public int player1PhysicalResist;
+    public int player1TechDamage;
+    public int player1TechResist;
+    public int player1EXPcurrent;
+    public int player1EXPneeded;
+    public int player1level;
+
+    //Player2 stats
+    public string player2Name;
+    public int player2HPMax;
+    public int player2HPCurrent;
+    public int player2PhysicalDamage;
+    public int player2PhysicalResist;
+    public int player2TechDamage;
+    public int player2TechResist;
+    public int player2EXPcurrent;
+    public int player2EXPneeded;
+    public int player2level;
+
+    //Player3 stats
+    public string player3Name;
+    public int player3HPMax;
+    public int player3HPCurrent;
+    public int player3PhysicalDamage;
+    public int player3PhysicalResist;
+    public int player3TechDamage;
+    public int player3TechResist;
+    public int player3EXPcurrent;
+    public int player3EXPneeded;
+    public int player3level;
+
+    //Player4 stats
+    public string player4Name;
+    public int player4HPMax;
+    public int player4HPCurrent;
+    public int player4PhysicalDamage;
+    public int player4PhysicalResist;
+    public int player4TechDamage;
+    public int player4TechResist;
+    public int player4EXPcurrent;
+    public int player4EXPneeded;
+    public int player4level;
+
+    //Player location
+    public float x;
+    public float y;
+    public float z;
 }
